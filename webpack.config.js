c84bb465b8dfcfbcd79bcb4fee2a04e53319db55
Nick Saunders <nick@saunde.rs>
2020-05-10 15:38:25 -0700

    Remove unnecessary indent ref.

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,6 @@ module.exports = (_, { mode }) => ({
         "https://unpkg.com/@hacss/plugin-delete@1.1.2/dist/hacss-plugin-delete.umd.min.js",
         "https://unpkg.com/@hacss/plugin-expand@1.1.1/dist/hacss-plugin-expand.umd.min.js",
         "https://unpkg.com/@hacss/plugin-variables@1.2.0/dist/hacss-plugin-variables.umd.min.js",
-        "https://unpkg.com/indent.js@0.3.5/lib/indent.min.js",
         "autoprefixer.js",
       ],
       append: false,
